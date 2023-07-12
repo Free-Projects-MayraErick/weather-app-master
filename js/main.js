2c534358d1490aecc03e418f5ff2d745b636cae0
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-07-11 19:48:47 -0600

    Consumiendo API con try catch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+const APIKEY = '7fe9d7ced8194f6aaf0619a754b0f7e1';
+const weatherAPI = async () => {
+    try{
+        const respuesta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Mérida&appid=${APIKEY}`);
+        console.log(respuesta); 
+        const data = respuesta.json(); //Esto permite recuperar la información devuelta por el json
+        console.log(data);
+    }catch(error){
+        console.log(error);
+    }
+
+  }
+  
+  weatherAPI();
